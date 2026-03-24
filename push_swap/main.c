552b0d75cfd99c1de701a3dc0e976978dae2fac7
GraciousYoong <graciousyoong@gmail.com>
2026-03-25 02:59:16 +0800

    updated main.c

--- push_swap/main.c
+++ push_swap/main.c
@@ -25,7 +25,7 @@ int	main(int argc, char **argv)
 	else if (argc == 2)
 		argv = split_arg(argv[1], ' ');
 	init_stack_a(&stack_a, argv + 1, argc == 2);
-	if (!stack_sorted(stack_a))
+	if (!stack_sorted(&stack_a))
 	{
 		if ((stack_a.size) == 2)
 			sa(&stack_a, false);
